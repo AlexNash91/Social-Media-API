02c39d347eb09b0ae7dbc144eba6a2bfd893293a
Alex Nash <Alexr.nash@gmail.com>
2023-02-25 18:00:07 -0500

    set up initial model code

---
+++ models/thought.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const thoughtScema = new mongoose.Schema({
+    thoughtText: { type: String, required: true},
+    createdAt: 
+    username: {type: String, required: true},
+    reactions: 
+})
