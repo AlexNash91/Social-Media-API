5733c436f3ce559fd51c241dae792faafe139f4c
Alex Nash <Alexr.nash@gmail.com>
2023-02-25 18:03:06 -0500

    added reaction.js + fixed typos

---
+++ models/reaction.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: 
+    reactionBody: {type: String, required: true},
+    username: {type: String, required: true},
+    createdAt:
+})
--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const thoughtScema = new mongoose.Schema({
+const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true},
     createdAt: 
     username: {type: String, required: true},
