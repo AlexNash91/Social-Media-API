57deb001eb906665fd829126857481fa3b87bf5f
Alex Nash <Alexr.nash@gmail.com>
2023-02-25 18:21:37 -0500

    little more progress on models

--- models/reaction.js
+++ models/reaction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const reactionSchema = new mongoose.Schema({
     reactionId: {type: mongoose.Types.ObjectId, default: new ObjectId},
-    reactionBody: {type: String, required: true},
+    reactionBody: {type: String, required: true, maxLength: 280},
     username: {type: String, required: true},
     createdAt: {type: Date, default: Date.now}
 })
--- models/thought.js
+++ models/thought.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const thoughtSchema = new mongoose.Schema({
-    thoughtText: { type: String, required: true},
-    createdAt: 
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280},
+    createdAt: {type: Date},
     username: {type: String, required: true},
     reactions: 
 })
