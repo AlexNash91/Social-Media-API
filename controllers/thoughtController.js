831a884050595f9dfde79953f063a86c0987a015
Alex Nash <Alexr.nash@gmail.com>
2023-02-28 22:27:06 -0500

    set up some starter code for coontrollers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,26 @@
 const { Thoughts, User} = require('../models')
 
 module.exports = {
+    getAllThoughts(req, res) {
+
+    },
+    getSingleThought(req, res) {
+
+    },
+    newThought(req, res) {
+
+    },
+    editThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    createReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    }
     
-}
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,26 @@
 const { Thoughts, User} = require('../models')
 
 module.exports = {
-    
-}
+    getAllUsers(req, res) {
+
+    },
+    getSingleUser(req, res) {
+
+    },
+    newUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    deleteFriend(req, res) {
+
+    }
+
+};
