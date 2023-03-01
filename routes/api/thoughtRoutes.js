37bc56461665e42c1056c8fa567fe444848176ee
Alex Nash <Alexr.nash@gmail.com>
2023-02-28 22:20:02 -0500

    set up routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getSingleThought,
+    newThought,
+    editThought,
+    deleteThought,
+    createReaction,
+    deleteReaction, 
+} = require('../../controllers/thoughtController');
 
+router.route('/api/thoughts').get(getAllThoughts).get(getSingleThought).post(newThought).put(editThought).delete(deleteThought)
 
-
-
-
-
-
-
-
-
+router.route('/api/thoughts/:thoughtId/reactions').post(createReaction).delete(deleteReaction)
 
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getSingleUser,
+    newUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+    
+} = require('../../controllers/userController');
 
+router.route('/api/users').get(getAllUsers).get(getSingleUser).post(newUser).put(updateUser).delete(deleteUser)
 
-
-
-
-
-
-
-
-
-
+router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
 
 
 module.exports = router;
