ebb3d6653ca69824a75c9be42bd85b69ad65de2e
Alex Nash <Alexr.nash@gmail.com>
2023-02-28 20:04:37 -0500

    changed reaction model into subdocument

--- models/thought.js
+++ models/thought.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {type: mongoose.Types.ObjectId, default: new ObjectId},
+    reactionBody: {type: String, required: true, maxLength: 280},
+    username: {type: String, required: true},
+    createdAt: {type: Date, default: () => Date.now}
+})
+
+
+
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280},
     createdAt: {type: Date, default: () => Date.now()},
     username: {type: String, required: true},
-    reactions: [String]
+    reactions: [reactionSchema]
 })
 
 thoughtSchema.virtual('reactionCount').get(function() {
