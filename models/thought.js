85359672d488aacda3e8db1a84aae867b6f47aec
Alex Nash <Alexr.nash@gmail.com>
2023-03-01 22:37:51 -0500

    got seeding to work

--- models/thought.js
+++ models/thought.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const reactionSchema = require('./reaction')
+const dateFormat = require('../utils/dateFormat')
 
-
-const reactionSchema = new mongoose.Schema({
-    reactionId: {type: mongoose.Types.ObjectId, default: new ObjectId},
-    reactionBody: {type: String, required: true, maxLength: 280},
-    username: {type: String, required: true},
-    createdAt: {type: Date, default: () => Date.now}
-})
 
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280},
@@ -19,5 +14,5 @@ thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length
 })
 
-const Thoughts = model('Thoughts', thoughtSchema)
+const Thoughts = mongoose.model('Thoughts', thoughtSchema)
 module.exports = Thoughts;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
@@ -11,6 +11,6 @@ userSchema.virtual('friendCount').get(function() {
     return friends.length
 })
 
-const User = model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
