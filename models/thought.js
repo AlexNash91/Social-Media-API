6b7f291f4f8dbda2866a84f9c002768bf450f272
Alex Nash <Alexr.nash@gmail.com>
2023-03-04 21:37:38 -0500

    added JSON conversion to models

--- models/thought.js
+++ models/thought.js
@@ -4,13 +4,20 @@ const dateFormat = require('../utils/dateFormat')
 
 
 const thoughtSchema = new mongoose.Schema({
-    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280},
-    createdAt: {type: Date, default: () => Date.now()},
-    username: {type: String, required: true},
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: { type: Date, default: () => Date.now() },
+    username: { type: String, required: true },
     reactions: [reactionSchema]
-})
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
 
-thoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length
 })
 
--- models/user.js
+++ models/user.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
-    thoughts: { type: [String], ref: "Thoughts"},
-    friends: { type: [String], ref: "User"}
-})
+    thoughts: { type: [String], ref: "Thoughts" },
+    friends: { type: [String], ref: "User" }
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-userSchema.virtual('friendCount').get(function() {
+userSchema.virtual('friendCount').get(function () {
     return friends.length
 })
 
